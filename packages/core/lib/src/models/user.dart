e4c089c39d453043689e13d27d5f961f1d4ef5b0
David Dulce <114516037+arkantrust@users.noreply.github.com>
2025-05-16 17:24:27 -0500

    feat: add User model with serialization and deserialization
    - Created User model with fields: id, firstName, lastName, email, and avatarUrl.
    - Implemented JSON serialization and deserialization using json_serializable and freezed packages.

---
+++ packages/core/lib/src/models/user.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+/// {@template user}
+/// User model
+///
+/// [User.empty] represents an unauthenticated user.
+/// {@endtemplate}
+// dart run build_runner build -d
+@freezed
+abstract class User with _$User {
+  @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+  const factory User({
+    required String id,
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String avatarUrl,
+  }) = _User;
+
+  /// Empty user represents an unauthenticated user.
+  static const empty = User(id: '', firstName: '', lastName: '', email: '', avatarUrl: '');
+
+  factory User.fromJson(Map<String, Object?> json) => _$UserFromJson(json);
+}
